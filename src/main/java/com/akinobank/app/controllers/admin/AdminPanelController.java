5d86a41694eb3796bee59580337a9ba2ba8b2f1a
OussamaDev <oussama.lahmidi@icloud.com>
2020-04-15 18:15:32 +0100

    Added Admin Users view and adding user form, and main controller. !! Please pull latest changes to avoid code conflicts.

--- src/main/java/com/akinobank/app/controllers/admin/AdminPanelController.java
+++ src/main/java/com/akinobank/app/controllers/admin/AdminPanelController.java
@@ -1,10 +1,14 @@
 package com.akinobank.app.controllers.admin;
 
 
+import com.akinobank.app.models.User;
 import com.akinobank.app.repositories.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,8 +19,28 @@ public class AdminPanelController {
 
 
     @GetMapping("")
-    public String index() {
+    public String index(Model model) {
         return ADMIN_VIEWS_PATH + "index";
     }
+
+    @GetMapping("users")
+    public String usersView(Model model) {
+        return ADMIN_VIEWS_PATH + "users";
+    }
+    @GetMapping("users/ajouter")
+    public String addUserView(Model model) {
+        /// juste pour tester le form.
+        User test = User.builder().email("test").password("hello").build();
+        model.addAttribute("user", test);
+        return ADMIN_VIEWS_PATH + "forms/adduser";
+    }
+    @PostMapping("users/ajouter")
+    public String greetingSubmit(@ModelAttribute User user) {
+        /// juste pour tester le form.
+        return ADMIN_VIEWS_PATH + "result";
+    }
+
+
+
 
 }
